e84e7d5ed5ca3c0673c6142c81ef9884e96b5055
Dahiams <carli32511489@gmail.com>
2026-01-02 12:46:12 -0400

    Mi 1er proyecto con Express !!

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require (`express`);
+const app  = express();
+const port = process.env.PORT || 3000;
+ 
+ app.set (`view engine`, `ejs`);
+ app.get('/', (req, res) => {
+    
+    res.render('index', { 
+        nombre: " GINNY ", 
+        cedula: "31.523.456", 
+        seccion: "Sección 4"
+});
+  });
+
+  app.listen (port,() => { 
+   
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+
+});
